302f17f86887e2b4aad813b3813c44c2fbbbe6a9
hafsahayath <44601750+hafsahayath@users.noreply.github.com>
2022-08-07 19:15:39 +0530

    refactor: set up props and types for table

--- src/components/react-table/Table.tsx
+++ src/components/react-table/Table.tsx
@@ -1,4 +1,14 @@
-const Table = () => {
+import { Column } from "react-table";
+
+interface ITableProps<T extends object> {
+  data: T[];
+  columns: Column<T>[];
+}
+
+const Table = <T extends object>({
+  columns,
+  data,
+}: ITableProps<T>): JSX.Element => {
   return <div>Table</div>;
 };
 
