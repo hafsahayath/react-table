233da8042a621cca9f0d3e15b0aa3be619d82185
hafsahayath <44601750+hafsahayath@users.noreply.github.com>
2022-08-07 19:24:55 +0530

    refactor: add columns file and memoize columns and data

---
+++ src/components/TableContainer/columns.tsx
@@ -0,0 +1,26 @@
+export const columns = [
+  {
+    Header: "ID",
+    accessor: "id",
+  },
+  {
+    Header: "First Name",
+    accessor: "first_name",
+  },
+  {
+    Header: "Last Name",
+    accessor: "last_name",
+  },
+  {
+    Header: "DOB",
+    accessor: "date_of_birth",
+  },
+  {
+    Header: "country",
+    accessor: "Country",
+  },
+  {
+    Header: "phone",
+    accessor: "Phone",
+  },
+];
--- src/components/TableContainer/index.tsx
+++ src/components/TableContainer/index.tsx
@@ -1,4 +1,11 @@
+import mockData from "./mock_data.json";
+import { columns as COLUMNS } from "./columns";
+import { useMemo } from "react";
+
 const TableContainer = (): JSX.Element => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => mockData, []);
+
   return <div>TableContainer</div>;
 };
 
