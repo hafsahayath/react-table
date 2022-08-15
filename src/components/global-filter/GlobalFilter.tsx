324da99fe90bebe6140fece6afc7820e68523a42
hafsahayath <44601750+hafsahayath@users.noreply.github.com>
2022-08-15 20:14:01 +0530

    refactor: use debounce for rapid table state changes

--- src/components/global-filter/GlobalFilter.tsx
+++ src/components/global-filter/GlobalFilter.tsx
@@ -1,12 +1,30 @@
-const GlobalFilter = ({ filter, setFilter }: {filter: string; setFilter: (arg0: string)=>void;}) => {
+import { useState } from "react";
+import { useAsyncDebounce } from "react-table";
+
+const GlobalFilter = ({
+  filter,
+  setFilter,
+}: {
+  filter: string;
+  setFilter: (arg0: string) => void;
+}) => {
+  const [value, setValue] = useState(filter);
+
+  const handleChange = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 300);
+
   return (
     <div>
       <label htmlFor="search">Search </label>
       <input
         type="text"
         id="search"
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+          handleChange(e.target.value);
+        }}
       />
     </div>
   );
