12fa6b066c98325362b607b01ae77a58ab99dfc5
michaelronald <michaelronald.dr@gmail.com>
2022-08-24 13:08:05 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <TableContainer />
+      <div>Test/</div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// You are stupid!
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
