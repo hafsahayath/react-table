92edd12a1b69cae3bdf4e005648110cee5e52541
hafsahayath <44601750+hafsahayath@users.noreply.github.com>
2022-09-18 22:00:51 +0530

    add jump to first and last page

--- src/components/react-table/PaginatedTable.tsx
+++ src/components/react-table/PaginatedTable.tsx
@@ -21,6 +21,8 @@ const PaginatedTable = <T extends object>({
     canPreviousPage,
     pageOptions,
     state,
+    gotoPage,
+    pageCount,
   } = useTable(
     {
       columns,
@@ -30,8 +32,6 @@ const PaginatedTable = <T extends object>({
   );
 
   const { pageIndex } = state;
-
-  // console.log({ pageOptions, pageIndex });
 
   return (
     <>
@@ -67,18 +67,31 @@ const PaginatedTable = <T extends object>({
         </tbody>
       </table>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <button onClick={previousPage} disabled={!canPreviousPage}>
-          previous
-        </button>
+        <div>
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {"<<"}
+          </button>
+          <button onClick={previousPage} disabled={!canPreviousPage}>
+            previous
+          </button>
+        </div>
         <span>
           Page{" "}
           <strong>
             {pageIndex + 1} of {pageOptions.length}
           </strong>
         </span>
-        <button onClick={nextPage} disabled={!canNextPage}>
-          next
-        </button>
+        <div>
+          <button onClick={nextPage} disabled={!canNextPage}>
+            next
+          </button>
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {">>"}
+          </button>
+        </div>
       </div>
     </>
   );
